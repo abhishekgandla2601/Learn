10d6ef8824769306753f561d13d2ef9371ad0c22
Abhishek <gandlaabhishek26work@gmail.com>
2025-12-20 21:09:24 +0530

    Create learn_1.py
    Stopped??

---
+++ practice/python/Task/Task_7/sets/learn_1.py
@@ -0,0 +1,12 @@
+# creating a set
+# creating an empty set
+st = set()
+
+#creating a set with initial items
+st = {'item1', 'item2', 'item3', 'item4'}
+
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+
+# getting sets lengths
+
+print(len(st))
