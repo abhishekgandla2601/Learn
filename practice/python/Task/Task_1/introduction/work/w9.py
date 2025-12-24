7b1b90eb6276c7db9e61f7bd9fc4ae77328b5b8f
Abhishek <gandlaabhishek26work@gmail.com>
2025-12-24 15:55:29 +0530

    Create w9.py
    New update

---
+++ practice/python/Task/Task_1/introduction/work/w9.py
@@ -0,0 +1,9 @@
+# Define two variables, j and k, with values 37 and 52 respectively.
+j, k = 37, 52
+# a) What is the sum of `j` and `k`?
+print("Sum of 'j' and 'k' : ", j+k)
+# b) Now re-assign `j` and `k` to have the vales 8 and 3 respectively.
+# Re-use your code from the editor to determine their sum and product.
+j, k = 8, 3
+print("Sum of 'j' and 'k' : ", j+k,"\nProduct of 'j' and 'k' : ", j*k)
+ 
